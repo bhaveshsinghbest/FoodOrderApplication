9db09a4e95c554da48bbd6e628dbdf27c49e8b24
bhaveshsinghbest <bhaveshsinghbest111@gmail.com>
2024-04-18 01:02:45 +0530

    Updated upto video 8

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,16 +8,96 @@ export class FoodService {
 
   constructor() { }
 
-getAll():string[]{
+getAll():Foods[]{
   return [
-    '/assets/1.jpeg',
-    '/assets/2.jpeg',
-    '/assets/3.jpeg',
-    '/assets/4.jpeg',
-    '/assets/5.jpeg',
-    '/assets/6.jpeg',
-    '/assets/7.jpeg',
-    '/assets/8.jpeg'
+   {
+    id: 1,
+    price: 100,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/1.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
+   {
+    id: 2,
+    price: 200,
+    name: 'Burger',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/2.jpeg",
+    cookTime: '30-40',
+    origins: ['Italy'],
+   },
+   {
+    id: 3,
+    price: 300,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/3.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
+   {
+    id: 4,
+    price: 400,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/4.jpeg",
+    cookTime: '10-20',
+    origins: ['Japan'],
+   },
+   {
+    id: 5,
+    price: 500,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/5.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
+   {
+    id: 6,
+    price: 600,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/6.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
+   {
+    id: 7,
+    price: 700,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/7.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
+   {
+    id: 8,
+    price: 800,
+    name: 'Pizza',
+    favorite:false,
+    star:4.5,
+    tags: ['Fast food','Pizza','Lunch'],
+    imageUrl: "/assets/8.jpeg",
+    cookTime: '10-20',
+    origins: ['Italy'],
+   },
   ]
 }
 }
