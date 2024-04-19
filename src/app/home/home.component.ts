a563c34f1f82025222724016591af86a563b70b2
bhaveshsinghbest <bhaveshsinghbest111@gmail.com>
2024-04-20 01:53:12 +0530

    Updated upto 10 videos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 import { StarRatingComponent } from 'ng-starrating';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,18 @@ export class HomeComponent implements OnInit {
 
   foods:Foods[]=[];
 
-  constructor(private fs:FoodService) { }
+  constructor(private fs:FoodService ,private router:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.foods=this.fs.getAll()
+
+    this.router.params.subscribe(params=>{
+      if(params['searchItem'])
+        this.foods=this.fs.getAll().filter(food=>food.name.toLowerCase().includes(params['searchItem'].toLowerCase()))
+      else
+      this.foods=this.fs.getAll()
+    })
+
+   
   }
 
 }
