fa1fd995238a577f3fc31c9ac1421de22bc36b8f
bhaveshsinghbest <bhaveshsinghbest111@gmail.com>
2024-05-04 01:25:49 +0530

    Completed all videos

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -3,125 +3,121 @@ import { Foods } from 'src/app/shared/models/food';
 import { Tag } from 'src/app/shared/models/tag';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
+  constructor() {}
 
-  constructor() { }
-
-  getFoodById(id:number):Foods{
-    return this.getAll().find(food=>food.id==id)!;
+  getFoodById(id: number): Foods {
+    return this.getAll().find((food) => food.id == id)!;
   }
 
-  getAllFoodByTag(tag:string):Foods[]{
-    return tag=='All'?
-     this.getAll(): this.getAll().filter(food=>food.tags?.includes(tag));
+  getAllFoodByTag(tag: string): Foods[] {
+    return tag == 'All'
+      ? this.getAll()
+      : this.getAll().filter((food) => food.tags?.includes(tag));
   }
 
-  getAllTag():Tag[]{
+  getAllTag(): Tag[] {
     return [
-      {name:"pizza",count:8},
-      {name:"burger",count:5},
-      {name:"Lunch",count:6},
-      {name:"Dinner",count:13},
-      {name:"fastfood",count:19},
-      {name:"Golgappa",count:9},
-      {name:"oizzapuff",count:3},
-      {name:"drink",count:5}
+      { name: 'Fast food', count: 4 },
+      { name: 'Non-Vegetarian', count: 2 },
+      { name: 'Vegetarian', count: 6 },
+      { name: 'Fruits', count: 1 },
+      { name: 'Chinese', count: 2 },
     ];
-
   }
 
-getAll():Foods[]{
-  return [
-   {
-    id: 1,
-    price: 100,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Pizza','Lunch'],
-    imageUrl: "/assets/1.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-   {
-    id: 2,
-    price: 200,
-    name: 'Burger',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food'],
-    imageUrl: "/assets/2.jpeg",
-    cookTime: '30-40',
-    origins: ['Italy'],
-   },
-   {
-    id: 3,
-    price: 300,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Pizza','Lunch'],
-    imageUrl: "/assets/3.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-   {
-    id: 4,
-    price: 400,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Lunch'],
-    imageUrl: "/assets/4.jpeg",
-    cookTime: '10-20',
-    origins: ['Japan'],
-   },
-   {
-    id: 5,
-    price: 500,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Pizza','Lunch'],
-    imageUrl: "/assets/5.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-   {
-    id: 6,
-    price: 600,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Pizza','Lunch'],
-    imageUrl: "/assets/6.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-   {
-    id: 7,
-    price: 700,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Pizza'],
-    imageUrl: "/assets/7.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-   {
-    id: 8,
-    price: 800,
-    name: 'Pizza',
-    favorite:false,
-    star:4.5,
-    tags: ['Fast food','Pizza','Lunch'],
-    imageUrl: "/assets/8.jpeg",
-    cookTime: '10-20',
-    origins: ['Italy'],
-   },
-  ]
-}
+  getAll(): Foods[] {
+    return [
+      {
+        id: 1,
+        price: 500,
+        name: 'Pizza',
+        favorite: false,
+        star: 4,
+        tags: ['Fast food', 'Vegetarian'],
+        imageUrl: '/assets/1.jpeg',
+        cookTime: '20-30',
+        origins: ['American'],
+      },
+      {
+        id: 2,
+        price: 100,
+        name: 'Fish',
+        favorite: false,
+        star: 4.5,
+        tags: ['Non-Vegetarian'],
+        imageUrl: '/assets/2.jpeg',
+        cookTime: '40-50',
+        origins: ['Indian', 'Srilanka'],
+      },
+      {
+        id: 3,
+        price: 800,
+        name: 'Mutton',
+        favorite: false,
+        star: 4.8,
+        tags: ['Non-Vegetarian'],
+        imageUrl: '/assets/3.jpeg',
+        cookTime: '50-55',
+        origins: ['Indian', 'USA', 'Russia'],
+      },
+      {
+        id: 4,
+        price: 50,
+        name: 'Paani puri',
+        favorite: false,
+        star: 4.5,
+        tags: ['Fast food', 'Vegetarian'],
+        imageUrl: '/assets/4.jpeg',
+        cookTime: '10-15',
+        origins: ['Indian', 'Nepal', 'Srilanla'],
+      },
+      {
+        id: 5,
+        price: 100,
+        name: 'Chowmein',
+        favorite: false,
+        star: 4.5,
+        tags: ['Fast food', 'Chinese', 'Vegetarian'],
+        imageUrl: '/assets/5.jpeg',
+        cookTime: '15-20',
+        origins: ['Italy', 'China'],
+      },
+      {
+        id: 6,
+        price: 150,
+        name: 'Mango',
+        favorite: false,
+        star: 5,
+        tags: ['Fruits', 'Vegetarian'],
+        imageUrl: '/assets/6.jpeg',
+        cookTime: '10-20',
+        origins: ['India'],
+      },
+      {
+        id: 7,
+        price: 80,
+        name: 'Momos',
+        favorite: false,
+        star: 3,
+        tags: ['Fast food', 'Chinese', 'Vegetarian'],
+        imageUrl: '/assets/7.jpeg',
+        cookTime: '10-20',
+        origins: ['India', 'China'],
+      },
+      {
+        id: 8,
+        price: 300,
+        name: 'Indian Thali',
+        favorite: false,
+        star: 5,
+        tags: ['Vegetarian'],
+        imageUrl: '/assets/8.jpeg',
+        cookTime: '20-30',
+        origins: ['India'],
+      },
+    ];
+  }
 }
